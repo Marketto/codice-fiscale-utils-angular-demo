e7cc52550c368f657e6e2fe2c4be0893a5573bff
Marco <marco.ricupero@gmail.com>
2020-01-12 20:28:38 +0100

    up

--- src/app/validators/mismatch-validation-fn.type.ts
+++ src/app/validators/mismatch-validation-fn.type.ts
@@ -1,10 +1,8 @@
-import ValidationError from './validation-error.type';
-import { CFMismatchValidator } from '@marketto/codice-fiscale-utils';
+import ValidationError from "./validation-error.type";
+import { CFMismatchValidator } from "@marketto/codice-fiscale-utils";
 
-type MismatchValidationFn = (
+export type MismatchValidationFn = (
   cfValidator: CFMismatchValidator,
   codiceFiscaleFormControlValue: string,
   secondFieldControlValue: unknown
 ) => ValidationError;
-
-export default MismatchValidationFn;
